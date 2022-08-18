0bc67e364b23a49a7bf3aa942d28be4702c51f1b
Tarandeep100 <tarandeepsingh100@gmail.com>
2022-08-18 05:28:01 -0400

    updated patient js

--- patient-data/application/frontend/src/Components/Patient/Patient.js
+++ patient-data/application/frontend/src/Components/Patient/Patient.js
@@ -15,31 +15,31 @@ import { fetchReadDoctor, fetchReadPatient } from '../../api/Common';
 
 import { DataGrid } from '@mui/x-data-grid';
 const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'ID', headerName: 'ID', width: 90 },
     {
-        field: 'name',
+        field: 'Name',
         headerName: 'Doctor name',
         width: 150,
         editable: false,
     },
     {
-        field: 'speciality',
+        field: 'Speciality',
         headerName: 'Speciality',
         width: 110,
         editable: false,
-    }
-];
-
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    },
+    {
+        field: 'Dob',
+        headerName: 'DOB',
+        width: 110,
+        editable: false,
+    },
+    {
+        field: 'DocType',
+        headerName: 'Type',
+        width: 110,
+        editable: false,
+    },
 ];
 
 
@@ -213,13 +213,14 @@ function Patient() {
                 <AccordionDetails>
                     <Box sx={{ height: 400, width: '100%' }}>
                         <DataGrid
-                            rows={rows}
+                            getRowId = {(row) = row.ID}
+                            rows={docList}
                             columns={columns}
                             pageSize={5}
                             rowsPerPageOptions={[5]}
                             checkboxSelection
                             disableSelectionOnClick
-                            experimentalFeatures={{ newEditingApi: true }}
+                            // experimentalFeatures={{ newEditingApi: true }}
                         />
                     </Box>
                 </AccordionDetails>
