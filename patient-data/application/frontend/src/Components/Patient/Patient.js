b9631a0bc06ee8287787f8232886122570484d09
Tarandeep100 <tarandeepsingh100@gmail.com>
2022-08-18 14:04:00 -0400

    updated patient js

--- patient-data/application/frontend/src/Components/Patient/Patient.js
+++ patient-data/application/frontend/src/Components/Patient/Patient.js
@@ -124,7 +124,7 @@ function Patient() {
                 console.log(i, docJson[i]);
                 result.push([i, docJson[i]]);
             }
-                
+            console.log(result.ID);
             setDocList(result);
             await setTimeout(2000);
 
